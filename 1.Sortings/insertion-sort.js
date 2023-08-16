72c220c097f38ad5d7bb556962636bfb84a7f9fa
techolution <suryateja.kandukuru@techolution.com>
2023-08-16 10:09:53 +0530

    fix: insertion sort

--- 1.Sortings/insertion-sort.js
+++ 1.Sortings/insertion-sort.js
@@ -9,21 +9,21 @@
 // This is a function named "insertionSort" that takes an array "arr" as input and sorts it in ascending order using the insertion sort algorithm.
 
 function insertionSort(arr) {
-  // The outer loop iterates over the array from the first element (index 0) to the second-to-last element (index arr.length - 2).
-  for (let i = 0; i < arr.length - 1; i++) {
-    // Initialize a variable "j" with the value 1.
-    let j = 1;
+  // The outer loop iterates over the array from the first element (index 0) to the last element.
+  for (let i = 0; i < arr.length; i++) {
+    // Initialize a variable "j" with the value i.
+    let j = i;
 
     // The while loop runs as long as the element at the current index "j" is smaller than the element at the previous index "j - 1".
     // It keeps moving the element at index "j" backward until it finds the correct position in the sorted part of the array.
-    while (arr[j] < arr[j - 1]) {
+    while (j > 0 && arr[j] < arr[j - 1]) {
       // If the element at index "j" is smaller than the element at index "j - 1," swap them using a temporary variable "temp."
       let temp = arr[j - 1];
       arr[j - 1] = arr[j];
       arr[j] = temp;
 
       // Move the index "j" one position backward to continue the comparison and potential swapping process.
-      j++;
+      j--;
     }
   }
 
